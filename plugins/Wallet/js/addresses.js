NebulousLabs/Sia-UI

Reorganized wallet code to more sensible

plugins/Wallet/js/addresses.js
@@ -13,7 +13,7 @@ function updateAddrTxn(event) {
 	IPC.sendToHost('api-call', {
 		url: '/wallet/transactions/' + event.target.innerText,
 		type: 'GET',
-	}, 'update-history');
+	}, 'update-transactions');
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Address Page ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -128,6 +128,16 @@ function locationOf(element, array, start, end) {
 function addAddress(addressObject) {
 	addresses.splice(locationOf(addressObject, addresses) + 1, 0, addressObject);
 }
+
+// Address creation
+$('#create-address').click(function() {
+	tooltip('Creating...', this);
+	var call = {
+		url: '/wallet/address',
+		type: 'GET',
+	};
+	IPC.sendToHost('api-call', call, 'new-address');
+});
 
 // Add the new address and show it
 addResultListener('new-address', function(result) {

plugins/Wallet/js/buttons.js
@@ -1,105 +1,19 @@
 'use strict';
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Address Handling  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Address creation
-$('#create-address').click(function() {
-	tooltip('Creating...', this);
-	var call = {
-		url: '/wallet/address',
-		type: 'GET',
-	};
-	IPC.sendToHost('api-call', call, 'new-address');
-});
-
-// Button to display all wallet addresses
-$('#view-all-addresses').click(function() {
-	$('#address-list').children().show();
-});
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Transactions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Define send call
-function sendCoin(amount, address) {
-	var transaction = {
-		amount: amount.toString(),
-		destination: address,
-	};
-	IPC.sendToHost('api-call', {
-		url: '/wallet/siacoins',
-		type: 'POST',
-		args: transaction,
-	}, 'coin-sent');
-
-	// Reflect it asap
-	setTimeout(update, 100);
-}
-
-// Transaction has to be legitimate
-function validateTransaction(caller, callback) {
-	var amount = $('#transaction-amount').val();
-	var unit = $('#send-unit').val();
-	var total = new BigNumber(amount).times(unit);
-	var address = $('#transaction-address').val();
-
-	// Verify number
-	if (!isNumber(amount)) {
-		tooltip('Enter numeric amount of Siacoin to send!', caller);
-		return;
-	} 
-	// Verify balance
-	if (wallet.confirmedsiacoinbalance < total) {
-		tooltip('Balance too low!', caller);
-		return;
-	} 
-	// Verify address
-	if (!isAddress(address)) {
-		tooltip('Enter correct address to send to!', caller);
-		return;
-	}
-
-	callback(total, address);
-}
-
-// Button to send coin
-$('#send-money').click(function() {
-	validateTransaction(this, function() {
-		tooltip('Are you sure?', $('#confirm').get(0));
-		$('#confirm').removeClass('transparent');
-	});
-});
-
-// Button to confirm transaction
-$('#confirm').click(function() {
-	// If the button's transparent, don't do anything
-	if ($('#confirm').hasClass('transparent')) {
-		return;
-	}
-	validateTransaction(this, function(amount, address) {
-		tooltip('Sending...', $('#confirm').get(0));
-		sendCoin(amount, address);
-	});
-	$('#confirm').addClass('transparent');
-});
-
-// Transaction was sent
-addResultListener('coin-sent', function(result) {
-	notify('Transaction sent to network!', 'sent');
-	$('#transaction-amount').val('');
-});
-
-// Button to load all wallet transactions
-$('#view-all-transactions').click(function() {
-	tooltip('Loading all transactions', this);
-	IPC.sendToHost('api-call', {
-		url: '/wallet/transactions',
-		args: {
-			startheight: 0,
-			endheight: 1000000,
-		},		
-		type: 'GET',
-	}, 'update-history');
-});
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Capsule ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Get and use password from the UI's config.json
+function getPassword() {
+	IPC.sendToHost('config', {key: 'walletPassword'}, 'use-password');
+}
+IPC.on('use-password', function(event, pw) {
+	if (pw) {
+		unlock(pw);
+	} else {
+		$('#request-password').show();
+		$('#password-field').focus();
+	}
+});
+
 // Lock or unlock the wallet
 $('#lock-pod').click(function() {
 	var state = $('#lock-status').html();
@@ -115,6 +29,14 @@ $('#lock-pod').click(function() {
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Popups ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Save password to the UI's config.json
+function savePassword(pw) {
+	IPC.sendToHost('config', {
+		key: 'walletPassword',
+		value: pw,
+	});
+}
+
 // On popup upon entering an encrypted, locked wallet, enter password
 $('#enter-password').click(function() {
 	// Save password if checked
@@ -161,11 +83,28 @@ $('.save-password').click(function() {
 	}
 });
 
+// Exit popup
 $('.close').click(function() {
 	$(this).closest('.popup').hide();
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Load ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Load legacy wallet from 0.33 fork
+function loadLegacyWallet(filename, password) {
+	IPC.sendToHost('api-call', {
+		url: '/wallet/load/033x',
+		type: 'POST',
+		args: {
+			filepath: filename,
+			encryptionpassword: password,
+		},
+	}, 'load-wallet');
+}
+addResultListener('load-wallet', function(result) {
+	notify('Loaded Wallet', 'success');
+});
+
+// Load legacy wallet from 0.33 fork
 $('#load-legacy-wallet').click(function() {
 	var loadPath = IPC.sendSync('dialog', 'open', {
 		title: 'Legacy Wallet File Path',

plugins/Wallet/js/lifecycle.js
@@ -5,14 +5,22 @@ var refreshRate = 500; // half-second
 var finalRefreshRate = 1000 * 60 * 5; // five-minutes
 // Keeps track of if the view is shown
 var updating;
-// Keeps track of wallet status results
-var wallet = {};
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Updating  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Make API calls, sending a channel name to listen for responses
 function update() {
+	// Get wallet status and balance
 	IPC.sendToHost('api-call', '/wallet', 'update-status');
-
+	// Get list of wallet transactions
+	IPC.sendToHost('api-call', {
+		url: '/wallet/transactions',
+		type: 'GET',
+		args: {
+			startheight: 0,
+			// Superfluous endheight to get whole history
+			endheight: 1000000,
+		}
+	}, 'update-transactions');
 	// Get list of wallet addresses
 	IPC.sendToHost('api-call', {
 		url: '/wallet/addresses',
@@ -20,110 +28,10 @@ function update() {
 	}, 'update-addresses');
 
 	updating = setTimeout(update, refreshRate);
-}
 
-// Update wallet summary in header
-addResultListener('update-status', function(result) {
-	wallet = result;
-
-	// slow down after first successful call
+	// slow down after first call
 	refreshRate = finalRefreshRate;
-
-	// Show correct lock status.
-	if (!wallet.encrypted) {
-		setUnencrypted();
-	} else if (!wallet.unlocked) {
-		setLocked();
-	} else if (wallet.unlocked) {
-		setUnlocked();
-	}
-
-	// Update balance confirmed and uncomfirmed
-	var bal = convertSiacoin(wallet.confirmedsiacoinbalance);
-	var pend = convertSiacoin(wallet.unconfirmedincomingsiacoins).sub(convertSiacoin(wallet.unconfirmedoutgoingsiacoins));
-	if (wallet.unlocked && wallet.encrypted) {
-		$('#confirmed').show();
-		$('#unconfirmed').show();
-		$('#confirmed').html('Balance: ' + bal + ' S');
-		$('#unconfirmed').html('Pending: ' + pend + ' S');
-	} else {
-		$('#confirmed').hide();
-		$('#unconfirmed').hide();
-	}
-});
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Transactions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Append a transaction to Transactions list
-function appendTransaction(txn) {
-	// Add only new transactions
-	if (typeof(txn) === 'undefined') { return; }
-	if ($('#' + txn.transactionid).length !== 0) { return; }
-
-	// Compute transaction net amount
-	var amount = new BigNumber(0);
-	if (txn.inputs) {
-		txn.inputs.forEach( function(input) {
-			if (input.walletaddress) {
-				amount = amount.sub(input.value);
-			}
-		});
-	}
-	if (txn.outputs) {
-		txn.outputs.forEach( function(output) {
-			if (output.walletaddress) {
-				amount = amount.add(output.value);
-			}
-		});
-	}
-
-	// Add only non-zero transactions
-	amount = convertSiacoin(amount);
-	if (amount.equals(0)) {
-		return;
-	}
-
-	// Make transaction
-	var txnElement = $('#transactionbp').clone();
-	txnElement.id = txn.transactionid;
-
-	txnElement.timestamp = txn.confirmationtimestamp * 1000;
-	var timestamp = new Date(txn.confirmationtimestamp * 1000);
-	var time = timestamp.toLocaleString();
-
-	// Insert transaction values in UI
-	txnElement.find('.amount').html(amount + ' S');
-	txnElement.find('.txnid').html(txn.transactionid);
-	txnElement.find('.time').html(time);
-
-	// Set transaction type
-	if (amount < 0) {
-		txnElement.find('.send').show();
-		txnElement.find('.receive').hide();
-	} else {
-		txnElement.find('.send').hide();
-		txnElement.find('.receive').show();
-	}
-
-	// Display transaction
-	$('#transaction-list').append(txnElement);
-	txnElement.show();
 }
-
-// Update transaction history
-addResultListener('update-history', function(result) {
-	if (result.confirmedtransactions) {
-		// Reverse direction of transactions list (most recent first)
-		result.confirmedtransactions.reverse();
-		result.confirmedtransactions.forEach(function (txn) {
-			appendTransaction(txn);
-		});
-	}
-	// TODO Register unconfirmed transactions
-	/*if (result.unconfirmed) {
-		result.unconfirmedtransactions.forEach(function(processedtxn) {
-		});
-	}*/
-});
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Start/Stop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Called upon showing

plugins/Wallet/js/transactions.js
@@ -1,0 +1,168 @@
+'use strict';
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Updating  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Append a transaction to Transactions list
+function appendTransaction(txn) {
+	// Add only new transactions
+	if (typeof(txn) === 'undefined') { return; }
+	if ($('#' + txn.transactionid).length !== 0) { return; }
+
+	// Compute transaction net amount
+	var amount = new BigNumber(0);
+	if (txn.inputs) {
+		txn.inputs.forEach( function(input) {
+			if (input.walletaddress) {
+				amount = amount.sub(input.value);
+			}
+		});
+	}
+	if (txn.outputs) {
+		txn.outputs.forEach( function(output) {
+			if (output.walletaddress) {
+				amount = amount.add(output.value);
+			}
+		});
+	}
+
+	// Add only non-zero transactions
+	amount = convertSiacoin(amount);
+	if (amount.equals(0)) {
+		return;
+	}
+
+	// Make transaction
+	var txnElement = $('#transactionbp').clone();
+	txnElement.id = txn.transactionid;
+
+	txnElement.timestamp = txn.confirmationtimestamp * 1000;
+	var timestamp = new Date(txn.confirmationtimestamp * 1000);
+	var time = timestamp.toLocaleString();
+
+	// Insert transaction values in UI
+	txnElement.find('.amount').html(amount + ' S');
+	txnElement.find('.txnid').html(txn.transactionid);
+	txnElement.find('.time').html(time);
+
+	// Set transaction type
+	if (amount < 0) {
+		txnElement.find('.send').show();
+		txnElement.find('.receive').hide();
+	} else {
+		txnElement.find('.send').hide();
+		txnElement.find('.receive').show();
+	}
+
+	// Display transaction
+	$('#transaction-list').append(txnElement);
+	txnElement.show();
+}
+
+// Update transaction history
+addResultListener('update-transactions', function(result) {
+	if (result.confirmedtransactions) {
+		// Reverse direction of transactions list (most recent first)
+		result.confirmedtransactions.reverse();
+		result.confirmedtransactions.forEach(function (txn) {
+			appendTransaction(txn);
+		});
+	}
+	// TODO Register unconfirmed transactions
+	/*if (result.unconfirmed) {
+		result.unconfirmedtransactions.forEach(function(processedtxn) {
+		});
+	}*/
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Sending  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Define send call
+function sendCoin(amount, address) {
+	var transaction = {
+		amount: amount.toString(),
+		destination: address,
+	};
+	IPC.sendToHost('api-call', {
+		url: '/wallet/siacoins',
+		type: 'POST',
+		args: transaction,
+	}, 'coin-sent');
+
+	// Reflect it asap
+	setTimeout(update, 100);
+}
+
+// Transaction was sent
+addResultListener('coin-sent', function(result) {
+	notify('Transaction sent to network!', 'sent');
+	$('#transaction-amount').val('');
+});
+
+// Amount has to be a number
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+// Address has to be lowercase hex and 76 chars
+function isAddress(str) {
+	return str.match(/^[a-f0-9]{76}$/) !== null;
+}
+
+// Transaction has to be legitimate
+function validateTransaction(caller, callback) {
+	var amount = $('#transaction-amount').val();
+	var unit = $('#send-unit').val();
+	var total = new BigNumber(amount).times(unit);
+	var address = $('#transaction-address').val();
+
+	// Verify number
+	if (!isNumber(amount)) {
+		tooltip('Enter numeric amount of Siacoin to send!', caller);
+		return;
+	} 
+	// Verify balance
+	if (wallet.confirmedsiacoinbalance < total) {
+		tooltip('Balance too low!', caller);
+		return;
+	} 
+	// Verify address
+	if (!isAddress(address)) {
+		tooltip('Enter correct address to send to!', caller);
+		return;
+	}
+
+	callback(total, address);
+}
+
+// Button to send coin
+$('#send-money').click(function() {
+	validateTransaction(this, function() {
+		tooltip('Are you sure?', $('#confirm').get(0));
+		$('#confirm').removeClass('transparent');
+	});
+});
+
+// Button to confirm transaction
+$('#confirm').click(function() {
+	// If the button's transparent, don't do anything
+	if ($('#confirm').hasClass('transparent')) {
+		return;
+	}
+	validateTransaction(this, function(amount, address) {
+		tooltip('Sending...', $('#confirm').get(0));
+		sendCoin(amount, address);
+	});
+	$('#confirm').addClass('transparent');
+});
+
+// Button to load all wallet transactions
+$('#view-all-transactions').click(function() {
+	tooltip('Loading all transactions', this);
+	IPC.sendToHost('api-call', {
+		url: '/wallet/transactions',
+		args: {
+			startheight: 0,
+			endheight: 1000000,
+		},		
+		type: 'GET',
+	}, 'update-history');
+});
+

vir-felix/sia-frontend

plugins/Wallet/js/addresses.js
@@ -13,7 +13,7 @@ function updateAddrTxn(event) {
 	IPCRenderer.sendToHost('api-call', {
 		url: '/wallet/transactions/' + event.target.innerText,
 		type: 'GET',
-	}, 'update-history');
+	}, 'update-transactions');
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Address Page ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -128,6 +128,16 @@ function locationOf(element, array, start, end) {
 function addAddress(addressObject) {
 	addresses.splice(locationOf(addressObject, addresses) + 1, 0, addressObject);
 }
+
+// Address creation
+$('#create-address').click(function() {
+	tooltip('Creating...', this);
+	var call = {
+		url: '/wallet/address',
+		type: 'GET',
+	};
+	IPCRenderer.sendToHost('api-call', call, 'new-address');
+});
 
 // Add the new address and show it
 addResultListener('new-address', function(result) {

plugins/Wallet/js/buttons.js
@@ -1,83 +1,17 @@
 'use strict';
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Address Handling  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Address creation
-$('#create-address').click(function() {
-	tooltip('Creating...', this);
-	var call = {
-		url: '/wallet/address',
-		type: 'GET',
-	};
-	IPCRenderer.sendToHost('api-call', call, 'new-address');
-});
-
-// Button to display all wallet addresses
-$('#view-all-addresses').click(function() {
-	$('#address-list').children().show();
-});
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Transactions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Define send call
-function sendCoin(amount, address) {
-	var transaction = {
-		amount: amount.toString(),
-		destination: address,
-	};
-	IPCRenderer.sendToHost('api-call', {
-		url: '/wallet/siacoins',
-		type: 'POST',
-		args: transaction,
-	}, 'coin-sent');
-
-	// Reflect it asap
-	setTimeout(update, 100);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Capsule ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Get and use password from the UI's config.json
+function getPassword() {
+	IPCRenderer.sendToHost('config', {key: 'walletPassword'}, 'use-password');
 }
-
-// Transaction has to be legitimate
-function validateTransaction(caller, callback) {
-	var amount = $('#transaction-amount').val();
-	var unit = $('#send-unit').val();
-	var total = new BigNumber(amount).times(unit);
-	var address = $('#transaction-address').val();
-
-	// Verify number
-	if (!isNumber(amount)) {
-		tooltip('Enter numeric amount of Siacoin to send!', caller);
-		return;
-	} 
-	// Verify balance
-	if (wallet.confirmedsiacoinbalance < total) {
-		tooltip('Balance too low!', caller);
-		return;
-	} 
-	// Verify address
-	if (!isAddress(address)) {
-		tooltip('Enter correct address to send to!', caller);
-		return;
+IPCRenderer.on('use-password', function(event, pw) {
+	if (pw) {
+		unlock(pw);
+	} else {
+		$('#request-password').show();
+		$('#password-field').focus();
 	}
-
-	callback(total, address);
-}
-
-// Button to send coin
-$('#send-money').click(function() {
-	validateTransaction(this, function() {
-		tooltip('Are you sure?', $('#confirm').get(0));
-		$('#confirm').removeClass('transparent');
-	});
-});
-
-// Button to confirm transaction
-$('#confirm').click(function() {
-	// If the button's transparent, don't do anything
-	if ($('#confirm').hasClass('transparent')) {
-		return;
-	}
-	validateTransaction(this, function(amount, address) {
-		tooltip('Sending...', $('#confirm').get(0));
-		sendCoin(amount, address);
-	});
-	$('#confirm').addClass('transparent');
 });
 
 // Transaction was sent
@@ -99,7 +33,6 @@ $('#view-all-transactions').click(function() {
 	}, 'update-history');
 });
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Capsule ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Lock or unlock the wallet
 $('#lock-pod').click(function() {
 	var state = $('#lock-status').html();
@@ -115,6 +48,14 @@ $('#lock-pod').click(function() {
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Popups ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Save password to the UI's config.json
+function savePassword(pw) {
+	IPCRenderer.sendToHost('config', {
+		key: 'walletPassword',
+		value: pw,
+	});
+}
+
 // On popup upon entering an encrypted, locked wallet, enter password
 $('#enter-password').click(function() {
 	// Save password if checked
@@ -161,11 +102,28 @@ $('.save-password').click(function() {
 	}
 });
 
+// Exit popup
 $('.close').click(function() {
 	$(this).closest('.popup').hide();
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Load ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Load legacy wallet from 0.33 fork
+function loadLegacyWallet(filename, password) {
+	IPCRenderer.sendToHost('api-call', {
+		url: '/wallet/load/033x',
+		type: 'POST',
+		args: {
+			filepath: filename,
+			encryptionpassword: password,
+		},
+	}, 'load-wallet');
+}
+addResultListener('load-wallet', function(result) {
+	notify('Loaded Wallet', 'success');
+});
+
+// Load legacy wallet from 0.33 fork
 $('#load-legacy-wallet').click(function() {
 	var loadPath = IPCRenderer.sendSync('dialog', 'open', {
 		title: 'Legacy Wallet File Path',

plugins/Wallet/js/lifecycle.js
@@ -5,14 +5,22 @@ var refreshRate = 500; // half-second
 var finalRefreshRate = 1000 * 60 * 5; // five-minutes
 // Keeps track of if the view is shown
 var updating;
-// Keeps track of wallet status results
-var wallet = {};
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Updating  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Make API calls, sending a channel name to listen for responses
 function update() {
+	// Get wallet status and balance
 	IPCRenderer.sendToHost('api-call', '/wallet', 'update-status');
-
+	// Get list of wallet transactions
+	IPCRenderer.sendToHost('api-call', {
+		url: '/wallet/transactions',
+		type: 'GET',
+		args: {
+			startheight: 0,
+			// Superfluous endheight to get whole history
+			endheight: 1000000,
+		}
+	}, 'update-transactions');
 	// Get list of wallet addresses
 	IPCRenderer.sendToHost('api-call', {
 		url: '/wallet/addresses',
@@ -20,110 +28,10 @@ function update() {
 	}, 'update-addresses');
 
 	updating = setTimeout(update, refreshRate);
-}
 
-// Update wallet summary in header
-addResultListener('update-status', function(result) {
-	wallet = result;
-
-	// slow down after first successful call
+	// slow down after first call
 	refreshRate = finalRefreshRate;
-
-	// Show correct lock status.
-	if (!wallet.encrypted) {
-		setUnencrypted();
-	} else if (!wallet.unlocked) {
-		setLocked();
-	} else if (wallet.unlocked) {
-		setUnlocked();
-	}
-
-	// Update balance confirmed and uncomfirmed
-	var bal = convertSiacoin(wallet.confirmedsiacoinbalance);
-	var pend = convertSiacoin(wallet.unconfirmedincomingsiacoins).sub(convertSiacoin(wallet.unconfirmedoutgoingsiacoins));
-	if (wallet.unlocked && wallet.encrypted) {
-		$('#confirmed').show();
-		$('#unconfirmed').show();
-		$('#confirmed').html('Balance: ' + bal + ' S');
-		$('#unconfirmed').html('Pending: ' + pend + ' S');
-	} else {
-		$('#confirmed').hide();
-		$('#unconfirmed').hide();
-	}
-});
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Transactions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Append a transaction to Transactions list
-function appendTransaction(txn) {
-	// Add only new transactions
-	if (typeof(txn) === 'undefined') { return; }
-	if ($('#' + txn.transactionid).length !== 0) { return; }
-
-	// Compute transaction net amount
-	var amount = new BigNumber(0);
-	if (txn.inputs) {
-		txn.inputs.forEach( function(input) {
-			if (input.walletaddress) {
-				amount = amount.sub(input.value);
-			}
-		});
-	}
-	if (txn.outputs) {
-		txn.outputs.forEach( function(output) {
-			if (output.walletaddress) {
-				amount = amount.add(output.value);
-			}
-		});
-	}
-
-	// Add only non-zero transactions
-	amount = convertSiacoin(amount);
-	if (amount.equals(0)) {
-		return;
-	}
-
-	// Make transaction
-	var txnElement = $('#transactionbp').clone();
-	txnElement.id = txn.transactionid;
-
-	txnElement.timestamp = txn.confirmationtimestamp * 1000;
-	var timestamp = new Date(txn.confirmationtimestamp * 1000);
-	var time = timestamp.toLocaleString();
-
-	// Insert transaction values in UI
-	txnElement.find('.amount').html(amount + ' S');
-	txnElement.find('.txnid').html(txn.transactionid);
-	txnElement.find('.time').html(time);
-
-	// Set transaction type
-	if (amount < 0) {
-		txnElement.find('.send').show();
-		txnElement.find('.receive').hide();
-	} else {
-		txnElement.find('.send').hide();
-		txnElement.find('.receive').show();
-	}
-
-	// Display transaction
-	$('#transaction-list').append(txnElement);
-	txnElement.show();
 }
-
-// Update transaction history
-addResultListener('update-history', function(result) {
-	if (result.confirmedtransactions) {
-		// Reverse direction of transactions list (most recent first)
-		result.confirmedtransactions.reverse();
-		result.confirmedtransactions.forEach(function (txn) {
-			appendTransaction(txn);
-		});
-	}
-	// TODO Register unconfirmed transactions
-	/*if (result.unconfirmed) {
-		result.unconfirmedtransactions.forEach(function(processedtxn) {
-		});
-	}*/
-});
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Start/Stop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Called upon showing

plugins/Wallet/js/transactions.js
@@ -1,0 +1,168 @@
+'use strict';
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Updating  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Append a transaction to Transactions list
+function appendTransaction(txn) {
+	// Add only new transactions
+	if (typeof(txn) === 'undefined') { return; }
+	if ($('#' + txn.transactionid).length !== 0) { return; }
+
+	// Compute transaction net amount
+	var amount = new BigNumber(0);
+	if (txn.inputs) {
+		txn.inputs.forEach( function(input) {
+			if (input.walletaddress) {
+				amount = amount.sub(input.value);
+			}
+		});
+	}
+	if (txn.outputs) {
+		txn.outputs.forEach( function(output) {
+			if (output.walletaddress) {
+				amount = amount.add(output.value);
+			}
+		});
+	}
+
+	// Add only non-zero transactions
+	amount = convertSiacoin(amount);
+	if (amount.equals(0)) {
+		return;
+	}
+
+	// Make transaction
+	var txnElement = $('#transactionbp').clone();
+	txnElement.id = txn.transactionid;
+
+	txnElement.timestamp = txn.confirmationtimestamp * 1000;
+	var timestamp = new Date(txn.confirmationtimestamp * 1000);
+	var time = timestamp.toLocaleString();
+
+	// Insert transaction values in UI
+	txnElement.find('.amount').html(amount + ' S');
+	txnElement.find('.txnid').html(txn.transactionid);
+	txnElement.find('.time').html(time);
+
+	// Set transaction type
+	if (amount < 0) {
+		txnElement.find('.send').show();
+		txnElement.find('.receive').hide();
+	} else {
+		txnElement.find('.send').hide();
+		txnElement.find('.receive').show();
+	}
+
+	// Display transaction
+	$('#transaction-list').append(txnElement);
+	txnElement.show();
+}
+
+// Update transaction history
+addResultListener('update-transactions', function(result) {
+	if (result.confirmedtransactions) {
+		// Reverse direction of transactions list (most recent first)
+		result.confirmedtransactions.reverse();
+		result.confirmedtransactions.forEach(function (txn) {
+			appendTransaction(txn);
+		});
+	}
+	// TODO Register unconfirmed transactions
+	/*if (result.unconfirmed) {
+		result.unconfirmedtransactions.forEach(function(processedtxn) {
+		});
+	}*/
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Sending  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Define send call
+function sendCoin(amount, address) {
+	var transaction = {
+		amount: amount.toString(),
+		destination: address,
+	};
+	IPCRenderer.sendToHost('api-call', {
+		url: '/wallet/siacoins',
+		type: 'POST',
+		args: transaction,
+	}, 'coin-sent');
+
+	// Reflect it asap
+	setTimeout(update, 100);
+}
+
+// Transaction was sent
+addResultListener('coin-sent', function(result) {
+	notify('Transaction sent to network!', 'sent');
+	$('#transaction-amount').val('');
+});
+
+// Amount has to be a number
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+// Address has to be lowercase hex and 76 chars
+function isAddress(str) {
+	return str.match(/^[a-f0-9]{76}$/) !== null;
+}
+
+// Transaction has to be legitimate
+function validateTransaction(caller, callback) {
+	var amount = $('#transaction-amount').val();
+	var unit = $('#send-unit').val();
+	var total = new BigNumber(amount).times(unit);
+	var address = $('#transaction-address').val();
+
+	// Verify number
+	if (!isNumber(amount)) {
+		tooltip('Enter numeric amount of Siacoin to send!', caller);
+		return;
+	} 
+	// Verify balance
+	if (wallet.confirmedsiacoinbalance < total) {
+		tooltip('Balance too low!', caller);
+		return;
+	} 
+	// Verify address
+	if (!isAddress(address)) {
+		tooltip('Enter correct address to send to!', caller);
+		return;
+	}
+
+	callback(total, address);
+}
+
+// Button to send coin
+$('#send-money').click(function() {
+	validateTransaction(this, function() {
+		tooltip('Are you sure?', $('#confirm').get(0));
+		$('#confirm').removeClass('transparent');
+	});
+});
+
+// Button to confirm transaction
+$('#confirm').click(function() {
+	// If the button's transparent, don't do anything
+	if ($('#confirm').hasClass('transparent')) {
+		return;
+	}
+	validateTransaction(this, function(amount, address) {
+		tooltip('Sending...', $('#confirm').get(0));
+		sendCoin(amount, address);
+	});
+	$('#confirm').addClass('transparent');
+});
+
+// Button to load all wallet transactions
+$('#view-all-transactions').click(function() {
+	tooltip('Loading all transactions', this);
+	IPCRenderer.sendToHost('api-call', {
+		url: '/wallet/transactions',
+		args: {
+			startheight: 0,
+			endheight: 1000000,
+		},		
+		type: 'GET',
+	}, 'update-history');
+});
+